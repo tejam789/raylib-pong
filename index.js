47531053441c4c2f934169d8493019d3a440c685
tejamedida <tejam@thoughtworks.com>
2026-07-10 14:58:37 +0530

    Moves a simple ball object in the window

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const r = require('raylib');
 
+const WINDOW_WIDTH = 800;
+const WINDOW_HEIGHT = 450;
+const FPS = 60;
+
+const BACKGROUND_COLOUR = r.RAYWHITE;
+const BALL_COLOUR = r.RED;
+
+const drawNewFrame = (x) => {
+  r.DrawCircle(x, 10, 5, BALL_COLOUR);
+}
+
 function main() {
-  r.InitWindow(800, 450, 'raylib [core] example - basic window');
-  r.SetTargetFPS(60)
+  r.InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, 'Pong');
+  r.SetTargetFPS(FPS);
+  let circleX = 10;
 
   while (!r.WindowShouldClose()) {
     // Game loop code here
     r.BeginDrawing();
 
-    r.ClearBackground(r.RAYWHITE);
-    r.DrawText('Welcome to pong', 10, 20, 20, r.LIGHTGRAY);
+    r.ClearBackground(BACKGROUND_COLOUR);
+    drawNewFrame(circleX);
+    circleX += 1;
 
     r.EndDrawing();
   }
